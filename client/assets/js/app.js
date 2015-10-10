94efd3d159dd2052506f1911dd0746eed6a3dadb
Joshua Smith <joshsmith01@me.com>
2015-10-09 22:54:24 -0700

    Add many changes some are testing purposes some.
    As of this commit, I have included the files and functions that seem to be doing the work.

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
 
-  angular.module('application', [
+  var app =  angular.module('application', [
     'ui.router',
     'ngAnimate',
 
@@ -10,44 +10,100 @@
     'foundation',
     'foundation.dynamicRouting',
     'foundation.dynamicRouting.animations'
-  ])
-    .config(config)
-    .run(run)
+  ]);
+  app.config(config)
+  app.run(run)
 
 
+  // This controller works to use data submitted by user for use in this controller. -JMS
 
-    // This controller works to use data submitted by user for use in this controller. -JMS
-    .controller('MyFormCtrl', [function() {
-        this.user = {
-             name: ''
-        };
-        this.register = function() {
-           console.log('User clicked register', this.user);
-        };
-    }])
-    // This controller works to use data submitted by user for use in this controller. -JMS
+  app.controller('colorCalcCtrl', function($scope) {
+    $scope.list = [];
 
-    .controller('colorCalcCtrl', function($scope) {
-      $scope.list = [];
+    $scope.addCard = function() {
+      // Push the user generated hex colors to the history cards -JMS
+      $scope.list.push({bColor: $scope.bColor, rColor: $scope.rColor});
+      // Reset the input and current card to default color values -JMS
+      $scope.bColor = '';
+      $scope.rColor = '';
+    };
 
-      $scope.addCard = function() {
-        // Push the user generated hex colors to the history cards -JMS
-        $scope.list.push({bColor: $scope.bColor, rColor: $scope.rColor});
-        // Reset the input and current card to default color values -JMS
-        $scope.bColor = '';
-        $scope.rColor = '';
-      };
+    $scope.getColor = function () {
+      console.log('Hello from getColor');
+      testVar();
 
-    })
+      function getColorFunction(colorString, hslDifferences, mode) {
 
-    // Custom filter to reverse the order of the array so that the latest entry resides on top. -JMS
-    .filter('reverse', function() {
-      return function(items) {
-        return items.slice().reverse();
-      };
-    })
-    // Keep this semicolon -JMS
-    ;
+        // default mode: sass
+        var mode = typeof mode !== 'undefined' ? mode : "sass";
+
+        var colorFunction = colorString;
+
+        // H
+        if (hslDifferences[0] !== 0) { // if hue changes
+          var invH = hslDifferences[0] * -1;
+          var hueFunction = ( mode == "sass" ) ? "adjust-hue" : "spin";
+          colorFunction = hueFunction + "( " + colorFunction + ", " + invH + "deg )";
+        }
+
+        // S
+        if (hslDifferences[1] < 0) { // if second color is more saturated
+          var absS = Math.abs(hslDifferences[1]);
+          colorFunction = "saturate( " + colorFunction + ", " + absS + " )";
+        } else if (hslDifferences[1] > 0) { // if second color is less saturated
+          colorFunction = "desaturate( " + colorFunction + ", " + hslDifferences[1] + " )";
+        }
+
+        // L
+        if (hslDifferences[2] < 0) { // if second color is lighter
+          var absL = Math.abs(hslDifferences[2]);
+          colorFunction = "lighten( " + colorFunction + ", " + absL + " )";
+        } else if (hslDifferences[2] > 0) { // if second color is darker
+          colorFunction = "darken( " + colorFunction + ", " + hslDifferences[2] + " )";
+        }
+
+        console.log(hslDifferences);
+        return ( colorFunction );
+
+      }
+
+      function getColorDifferences(start, end) {
+
+        var differences = [];
+
+        var startColor = Color(start);
+        var endColor = Color(end);
+
+        var startColorHSL = startColor.hslArray();
+        var endColorHSL = endColor.hslArray();
+
+        for (var i = 0; i < 3; i++) {
+          differences[i] = startColorHSL[i] - endColorHSL[i];
+        }
+
+        return ( differences );
+
+      }
+
+      // Variables from the forked repo starting on line :57 -JMS
+      var startColor = $scope.rColor;
+      var endColor = $scope.rColor;
+      var differences = getColorDifferences(startColor, endColor);
+      var outputText = getColorFunction(startColor, differences);
+
+    };
+
+  })
+
+  // Custom filter to reverse the order of the array so that the latest entry resides on top. -JMS
+  app.filter('reverse', function() {
+    return function(items) {
+      return items.slice().reverse();
+    };
+  })
+  // Keep this semicolon -JMS
+  ;
+
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
 
